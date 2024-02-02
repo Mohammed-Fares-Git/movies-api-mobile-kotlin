2b159359217a4b6458e2c6cd8474a055083209b8
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-02-02 15:10:08 +0100

    create hilt android app an its module

--- app/src/main/java/com/mohammedfares/consumemoviesapi/di/MovieApiApplication.kt
+++ app/src/main/java/com/mohammedfares/consumemoviesapi/di/MovieApiApplication.kt
@@ -1,4 +1,8 @@
 package com.mohammedfares.consumemoviesapi.di
 
-class MovieApiApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieApiApplication: Application() {
 }
--- app/src/main/java/com/mohammedfares/consumemoviesapi/di/NetworkModule.kt
+++ app/src/main/java/com/mohammedfares/consumemoviesapi/di/NetworkModule.kt
@@ -1,4 +1,46 @@
 package com.mohammedfares.consumemoviesapi.di
 
+import android.content.Context
+import com.mohammedfares.consumemoviesapi.comon.AuthManager
+import com.mohammedfares.consumemoviesapi.comon.Constants
+import com.mohammedfares.consumemoviesapi.data.remote.AuthService
+import com.mohammedfares.consumemoviesapi.data.remote.MovieService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun provideMovieService(retrofit: Retrofit): MovieService {
+        return retrofit.create(MovieService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthService(retrofit: Retrofit): AuthService {
+        return retrofit.create(AuthService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthManager(@ApplicationContext context: Context): AuthManager {
+        return AuthManager(context)
+    }
 }
