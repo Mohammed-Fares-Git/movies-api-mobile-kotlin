42de56cf4853058a4ac75cf855cb9b83fa75433d
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-02-02 15:07:09 +0100

    create irepository and its implementation

--- app/src/main/java/com/mohammedfares/consumemoviesapi/data/RemoteRepositoryImpl.kt
+++ app/src/main/java/com/mohammedfares/consumemoviesapi/data/RemoteRepositoryImpl.kt
@@ -1,4 +1,28 @@
 package com.mohammedfares.consumemoviesapi.data
 
-class RemoteRepositoryImpl {
+import com.mohammedfares.consumemoviesapi.data.remote.AuthService
+import com.mohammedfares.consumemoviesapi.data.remote.MovieService
+import com.mohammedfares.consumemoviesapi.domain.models.AuthRequest
+import com.mohammedfares.consumemoviesapi.domain.models.AuthResponse
+import com.mohammedfares.consumemoviesapi.domain.models.Movie
+import com.mohammedfares.consumemoviesapi.domain.models.RegisterRequest
+import com.mohammedfares.consumemoviesapi.domain.repositories.IRepository
+import javax.inject.Inject
+
+class RemoteRepositoryImpl @Inject constructor(
+    val api: MovieService,
+    val auth: AuthService
+): IRepository {
+    override suspend fun registerUser(registerRequest: RegisterRequest): AuthResponse {
+        return auth.registerUser(registerRequest)
+    }
+
+    override suspend fun authenticateUser(authRequest: AuthRequest): AuthResponse {
+        return auth.authenticateUser(authRequest)
+    }
+
+    override suspend fun getAllMovies(): List<Movie> {
+        return api.getAllMovies()
+    }
+
 }
--- app/src/main/java/com/mohammedfares/consumemoviesapi/domain/repositories/IRepository.kt
+++ app/src/main/java/com/mohammedfares/consumemoviesapi/domain/repositories/IRepository.kt
@@ -1,4 +1,12 @@
 package com.mohammedfares.consumemoviesapi.domain.repositories
 
+import com.mohammedfares.consumemoviesapi.domain.models.AuthRequest
+import com.mohammedfares.consumemoviesapi.domain.models.AuthResponse
+import com.mohammedfares.consumemoviesapi.domain.models.Movie
+import com.mohammedfares.consumemoviesapi.domain.models.RegisterRequest
+
 interface IRepository {
+    suspend fun registerUser(registerRequest: RegisterRequest): AuthResponse
+    suspend fun authenticateUser(authRequest: AuthRequest): AuthResponse
+    suspend fun getAllMovies(): List<Movie>
 }
