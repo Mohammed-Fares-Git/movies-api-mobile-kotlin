5df3c26113188922287b6703eb6ca88ccbb5dbdf
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-02-02 15:08:09 +0100

    create retrofit interfaces

--- app/src/main/java/com/mohammedfares/consumemoviesapi/data/remote/AuthService.kt
+++ app/src/main/java/com/mohammedfares/consumemoviesapi/data/remote/AuthService.kt
@@ -1,4 +1,16 @@
 package com.mohammedfares.consumemoviesapi.data.remote
 
+import com.mohammedfares.consumemoviesapi.domain.models.AuthRequest
+import com.mohammedfares.consumemoviesapi.domain.models.AuthResponse
+import com.mohammedfares.consumemoviesapi.domain.models.RegisterRequest
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface AuthService {
+
+    @POST("auth/register")
+    suspend fun registerUser(@Body registerRequest: RegisterRequest): AuthResponse
+
+    @POST("/auth/authenticate")
+    suspend fun authenticateUser(@Body authRequest: AuthRequest): AuthResponse
 }
--- app/src/main/java/com/mohammedfares/consumemoviesapi/data/remote/MovieService.kt
+++ app/src/main/java/com/mohammedfares/consumemoviesapi/data/remote/MovieService.kt
@@ -1,4 +1,16 @@
 package com.mohammedfares.consumemoviesapi.data.remote
 
+import com.mohammedfares.consumemoviesapi.domain.models.AuthRequest
+import com.mohammedfares.consumemoviesapi.domain.models.AuthResponse
+import com.mohammedfares.consumemoviesapi.domain.models.Movie
+import com.mohammedfares.consumemoviesapi.domain.models.RegisterRequest
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface MovieService {
+
+    @GET("/movies/all")
+    suspend fun getAllMovies(): List<Movie>
+
 }
