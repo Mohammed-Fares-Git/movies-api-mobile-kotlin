fa05fc7912928c5fe24badcd0c7b1f00a42f872d
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-02-02 15:10:38 +0100

    create package for ui

--- app/src/main/java/com/mohammedfares/consumemoviesapi/presentation/screens/AuthScreen.kt
+++ app/src/main/java/com/mohammedfares/consumemoviesapi/presentation/screens/AuthScreen.kt
@@ -1,2 +1,24 @@
 package com.mohammedfares.consumemoviesapi.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AuthScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+    }
+}
