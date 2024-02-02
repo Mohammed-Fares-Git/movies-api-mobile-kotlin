1356321c2cb923441dcb252966ad3ecd06150f15
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-02-02 17:50:35 +0100

    add auth functionality

--- app/src/main/java/com/mohammedfares/consumemoviesapi/presentation/MainActivity.kt
+++ app/src/main/java/com/mohammedfares/consumemoviesapi/presentation/MainActivity.kt
@@ -3,27 +3,42 @@ package com.mohammedfares.consumemoviesapi.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import com.mohammedfares.consumemoviesapi.comon.Auth
+import com.mohammedfares.consumemoviesapi.presentation.screens.AuthScreen
+import com.mohammedfares.consumemoviesapi.presentation.screens.AuthScreenViewModel
+import com.mohammedfares.consumemoviesapi.presentation.screens.MovieScreen
 import com.mohammedfares.consumemoviesapi.presentation.ui.theme.ConsumeMoviesApiTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
+    val mainViewModel: MainViewModel by viewModels()
+    val authScreenViewModel: AuthScreenViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ConsumeMoviesApiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val auth = mainViewModel.authState.collectAsState()
+
+                when (auth.value){
+                    is Auth.Authenticated -> MovieScreen()
+                    Auth.UnAuthenticated -> AuthScreen(viewModel = authScreenViewModel)
                 }
+
             }
         }
     }
--- app/src/main/java/com/mohammedfares/consumemoviesapi/presentation/MainViewModel.kt
+++ app/src/main/java/com/mohammedfares/consumemoviesapi/presentation/MainViewModel.kt
@@ -1,4 +1,29 @@
 package com.mohammedfares.consumemoviesapi.presentation
 
-class MainViewModel {
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mohammedfares.consumemoviesapi.comon.Auth
+import com.mohammedfares.consumemoviesapi.domain.use_cases.AuthLocalyUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    val auth: AuthLocalyUseCase
+): ViewModel() {
+    init {
+        viewModelScope.launch {
+            auth().collect {
+                _AuthState.value = it
+            }
+        }
+    }
+
+    private val _AuthState = mutableStateOf<Auth>(Auth.UnAuthenticated)
+    val authState = _AuthState as StateFlow<Auth>
+
 }
